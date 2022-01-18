6de05727d6909e64174c398a44f398fc4220e9b7
bonjour-anuj <bonjour.anuj@gmail.com>
2022-01-18 16:05:58 +0530

    Project Scaffolding
    1. Adds simple webpack build config

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+  entry: './index.js',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{from: 'manifest.json', to: 'manifest.json'}],
+    })],
+};
