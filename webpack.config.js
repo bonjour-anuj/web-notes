e6c7338a1421a0f29d97cad6726f10bee921dca1
bonjour-anuj <bonjour.anuj@gmail.com>
2022-01-19 11:39:08 +0530

    Adds background service-worker & content script
    1. Adds vanilla background service worker
    2. Adds vanilla content script for all https urls
    3. Configures webpack for respective bundles along with non-eval source map.

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,17 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
   context: path.resolve(__dirname, 'src'),
-  entry: './index.js',
+  entry: {
+    app: './background/app.js',
+    init: './contentScript/init.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
   plugins: [
     new CopyPlugin({
       patterns: [{from: 'manifest.json', to: 'manifest.json'}],
